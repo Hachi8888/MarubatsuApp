b730f56332b605add273fe29595df0ca279c4137
Hachi8888 <h.nana.420@gmail.com>
2019-08-04 10:55:39 +0900

    後半編の拡張前段階

--- MarubatsuApp3/ViewController.swift
+++ MarubatsuApp3/ViewController.swift
@@ -36,23 +36,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    showQuestion()
+        
     }
     
     
     // 2,◯ボタンの紐付け
    
     @IBAction func tappedYesButton(_ sender: UIButton) {
+        //◯ボタンを押したときの動作
+        checkAnswer(yourAnswer: true)
         
     }
     
     // 2,xボタンの紐付け
     @IBAction func tappedNoButton(_ sender: UIButton) {
-        
+        //☓ボタンを押したときの動作
+        checkAnswer(yourAnswer: false)
     }
 
-
+    //アラートを表示する関数
+    func showAlert(message: String){
+        //アラートの作成
+        let alert = UIAlertController(title: "あなたの答えは..." , message: message, preferredStyle: .alert)
+        //アラートのアクション(ボタンの定義)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        //作成したalertに閉じるボタンを追加
+        alert.addAction(close)
+        //アラートを表示する
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     //問題を表示する関数
-    func shouQuestion(){
+    func showQuestion(){
         //currentQuestion(問題番号)の問題を取得
         let question = questions[currentQuestionNum]
         
@@ -63,10 +80,43 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    
-    
-    
+    // 正解なら次の問題を表示します
+    func checkAnswer(yourAnswer: Bool) {
+        
+        let question = questions[currentQuestionNum]
+        
+        if let ans = question["answer"] as? Bool {
+            
+            if yourAnswer == ans {
+                // 正解
+                // currentQuestionNumを1足して次の問題に進む
+               showAlert(message: "【祝】正解です")
+                currentQuestionNum += 1
+                
+                
+            } else {
+                // 不正解
+                showAlert(message: "不正解です")
+                currentQuestionNum += 1
+            }
+        }else{
+            print("答えが入ってません")
+            return
 }
+        
+        
 
+        
+        if currentQuestionNum >= questions.count {
+            currentQuestionNum = 0
+        }
+        
+        // 問題を表示します。
+        //正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
+        showQuestion()
+    }
+
+
+    }
+    
 

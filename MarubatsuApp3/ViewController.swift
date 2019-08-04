ba364d27748462df623fc1ff2ca67f0b2e3c0af8
Hachi8888 <h.nana.420@gmail.com>
2019-08-04 13:58:53 +0900

    大幅修正版(ただし、結果発表の表示がおかしいエラーあり

--- MarubatsuApp3/ViewController.swift
+++ MarubatsuApp3/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // 1、クイズラベルの紐付け
     @IBOutlet weak var questionLabel: UILabel!
-
+    
     // 3,現在のクイズ番号を管理
     var currentQuestionNum: Int = 0
     
@@ -20,35 +20,36 @@ class ViewController: UIViewController {
     var collectNum: Int = 0
     
     // 問題を管理
-    let questions: [String: Any : String] = [
-        [ "question": "1 + 1 = 2 である",
-          "answer": true,
-          "hint": "たんぼのた"
-        ],
-        [ "question": "石川県は四国地方である",
-          "answer": false,
-          "hint": "日本海に面しています"
-        ],
-        [ "question": "トマトは夏野菜である",
-          "answer": true,
-          "hint": "トマトと"
-        ],
-        [ "question": "つくば市は栃木県である",
-          "answer": false
-        ],
+    let questions: [[String: Any]] = [
+    [ "question": "1 + 1 = 2 である",
+    "answer": true,
+    "hint": "たんぼのた"
+    ],
+    [ "question": "石川県は四国地方である",
+    "answer": false,
+    "hint": "日本海に面しています"
+    ],
+    [ "question": "トマトは夏野菜である",
+    "answer": true,
+    "hint": "真っ赤な太陽がかがやく季節に実ります"
+    ],
+    [ "question": "つくば市は栃木県である",
+    "answer": false,
+    "hint": "秋葉原から45分!"
+    ],
     ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    showQuestion()
+        showQuestion()
         
     }
     
     
     // 2,◯ボタンの紐付け
-   
+    
     @IBAction func tappedYesButton(_ sender: UIButton) {
         //◯ボタンを押したときの動作
         checkAnswer(yourAnswer: true)
@@ -60,11 +61,11 @@ class ViewController: UIViewController {
         //☓ボタンを押したときの動作
         checkAnswer(yourAnswer: false)
     }
-
+    
     //アラートを表示する関数
     func showAlert(message: String){
         //アラートの作成
-        let alert = UIAlertController(title: "あなたの答えは..." , message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         //アラートのアクション(ボタンの定義)
         let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
         //作成したalertに閉じるボタンを追加
@@ -106,15 +107,9 @@ class ViewController: UIViewController {
     // ★正解数を表示するための関数
     func checkLastAnswer(){
         // 正解数の結果発表
-        let result = "\(questions.count)問中\(collectNum)問正解です!"
+        showAlert(message:"\(questions.count)問中\(collectNum)問正解です!")
         
-        if currentQuestionNum >= questions.count{
-            
-            // 正解数の結果発表をします
-            showCollectNum(message: result)
-            
-        }
-        }
+    }
     
     // 正解なら次の問題を表示します
     func checkAnswer(yourAnswer: Bool) {
@@ -127,57 +122,56 @@ class ViewController: UIViewController {
                 // 正解
                 // currentQuestionNumを1足して次の問題に進む
                 // 回答するとcurrentQuestionNumに1を足す(問題を進める)
-                currentQuestionNum += 1
+                
                 // ★正解するとcollectNumに1を足す(正解数のカウント)
-                collectNum += 1
+                
                 
                 //最終問題かチェック、そうならば結果を表示
-                if currentQuestionNum >=  questions.count{
+                if currentQuestionNum >=  questions.count - 1 {
                     checkLastAnswer()
+                    currentQuestionNum = 0
+                    return
                 } else {
-                 showAlert(message: "【祝】正解です")
+                    showAlert(message: "【祝】正解です")
+                    currentQuestionNum += 1
+                    collectNum += 1
+                }
+                
+            } else if yourAnswer != ans {
+                // 不正解
+                if currentQuestionNum >= questions.count - 1 {
+                    checkLastAnswer()
+                    currentQuestionNum = 0
+                    return
+                    
+                } else {
+                    showAlert(message: "不正解です")
+                    
+                    // 回答するとcurrentQuestionNumに1を足す(問題を進める)
+                    currentQuestionNum += 1
+                    
+                    
                 }
                 
             } else {
-                // 不正解
-                if currentQuestionNum >=  questions.count{
-                    checkLastAnswer()
-                } else {
-                    showAlert(message: "不正解です")
-                
-                // 回答するとcurrentQuestionNumに1を足す(問題を進める)
-                currentQuestionNum += 1
-                    
+                print("答えが入ってません")
+                return
             }
-            
-        }else{
-            print("答えが入ってません")
-            return
-}
-        
-        
-        
-
-        
-        if currentQuestionNum >= questions.count {
-
-        // 最初の問題に戻ります
-             currentQuestionNum = 0
-
-
-
         }
         
         
         
-        
-//         問題を表示します。
-//        正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
-         showQuestion()
-    }
-    
 
+        
+    
+        //         問題を表示します。
+        //        正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
+        showQuestion()
     }
     
+    
+}
+
+
 
 
